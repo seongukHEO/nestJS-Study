7707eda788f5d90075c8a9c6bf06ecc44c374c02
seongukHEO <heoseonguk3@gmail.com>
2025-05-13 02:41:03 +0900

    데코레이터에 Path 추가하기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -17,11 +17,14 @@ interface Post {
   commentCount: number;
 }
 
+//만약 Controller에도 post를 넣어주고
+//Get에도 post를 넣어준다면
+// /post/post를 해야 값이 나온다
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('post')
   getPost(): Post {
     return {
       author: 'Founder',
