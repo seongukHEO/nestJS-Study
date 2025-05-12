1a24b7f29563f08ede62d4eb024712ff58140667
seongukHEO <heoseonguk3@gmail.com>
2025-05-13 01:20:46 +0900

    post Json 반환하기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,34 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+/**
+ * author : String
+ * title : String
+ * content : String
+ * likeCount : number
+ * commentCount : number
+ */
+
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getPost(): Post {
+    return {
+      author: 'Founder',
+      title: '안녕하세요 FOUNDER입니다',
+      content: '원단 혁신은 FOUNDER에서',
+      likeCount: 100000,
+      commentCount: 999,
+    };
   }
 }
