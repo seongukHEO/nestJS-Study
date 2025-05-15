ba52209bb5fab3213031b69e9d36648c77bb9e17
seongukHEO <heoseonguk3@gmail.com>
2025-05-15 22:54:30 +0900

    GetPosts 구현하기

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -9,7 +9,8 @@ import { PostsService } from './posts.service';
  * commentCount : number
  */
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -17,19 +18,54 @@ interface Post {
   commentCount: number;
 }
 
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: '허성욱',
+    title: 'ㅎ호호호',
+    content: '허성욱은 바보',
+    likeCount: 1000,
+    commentCount: 1000,
+  },
+  {
+    id: 2,
+    author: '허성욱우우우',
+    title: 'ㅎ호호ㅇㅇㅇㅇ호',
+    content: '허성욱ㅇㅇㅇ은 바보',
+    likeCount: 1000,
+    commentCount: 1000,
+  },
+  {
+    id: 3,
+    author: '허성욱메롱',
+    title: 'ㅎ호호호우우우',
+    content: '허성욱은 어어어',
+    likeCount: 1000,
+    commentCount: 1000,
+  },
+];
+
 //자동으로 생성된다
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  //GET / posts
+  //  모든 posts를 가져온다
   @Get()
-  getPost(): Post {
-    return {
-      author: 'Founder',
-      title: '안녕하세요 FOUNDER입니다',
-      content: '원단 혁신은 FOUNDER에서',
-      likeCount: 100000,
-      commentCount: 999,
-    };
+  getPosts() {
+    return posts;
   }
+
+  //GET / posts/:id
+  //  id에 해당하는 post를 가져온다
+
+  //POST / posts
+  //  Post를 생성한다
+
+  //PUT /posts/:id
+  //  id에 해단되는 post를 변경한다
+
+  //DELETE /posts/:id
+  //  id에 해당하는 post를 삭제한다
 }
